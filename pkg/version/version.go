cfc9c56220d3937a8cda00f8561a6fa2964e4112
Dale-Kurt Murray <hello@dalekurtmurray.com>
2025-04-09 19:15:07 -0400

    chore: prepare release v0.1.4

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,11 +3,11 @@ package version
 // Version information
 var (
 	// Version is the current version of kube-ai
-	Version = "0.1.3"
+	Version = "0.1.4"
 
 	// GitCommit is the git commit that was compiled
-	GitCommit = "c9dce8f"
+	GitCommit = "022c805"
 
 	// BuildDate is the date when the binary was built
-	BuildDate = "2025-04-09T23:00:59Z"
+	BuildDate = "2025-04-09T23:15:07Z"
 )
