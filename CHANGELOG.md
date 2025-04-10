24a0369fa4c133a483bbcdb625eb297a7ed96db2
Dale-Kurt Murray <hello@dalekurtmurray.com>
2025-04-10 08:58:55 -0400

    chore: prepare release v0.1.8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.8] - 2025-04-10
+
 ## [0.1.7] - 2025-04-19
 
 ### Added
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,11 +3,11 @@ package version
 // Version information
 var (
 	// Version is the current version of kube-ai
-	Version = "0.1.7"
+	Version = "0.1.8"
 
 	// GitCommit is the git commit that was compiled
-	GitCommit = "4b92d0b"
+	GitCommit = "1c5b6be"
 
 	// BuildDate is the date when the binary was built
-	BuildDate = "2025-04-10T12:31:53Z"
+	BuildDate = "2025-04-10T12:58:55Z"
 )
