da32bc631a6d271df28f396700784947af6f3014
Dale-Kurt Murray <hello@dalekurtmurray.com>
2025-04-10 10:09:37 -0400

    chore: prepare release v0.1.8

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,8 +6,8 @@ var (
 	Version = "0.1.8"
 
 	// GitCommit is the git commit that was compiled
-	GitCommit = "5e5ff65"
+	GitCommit = "26a0676"
 
 	// BuildDate is the date when the binary was built
-	BuildDate = "2025-04-10T13:36:12Z"
+	BuildDate = "2025-04-10T14:09:37Z"
 )
