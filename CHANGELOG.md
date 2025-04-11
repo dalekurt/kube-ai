4db652e179ef8c88b3d40e8eeba4a1f90558eca1
Dale-Kurt Murray <hello@dalekurtmurray.com>
2025-04-10 20:24:29 -0400

    chore: prepare release v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [0.2.0] - 2025-04-10
 
+## [0.2.0] - 2025-04-10
+
 ### Added
 - feat: Implement seamless kubectl flag support across all commands (a4ae47f)
 - docs: Update CHANGELOG.md with log display feature (7b5cd35)
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,11 +3,11 @@ package version
 // Version information
 var (
 	// Version is the current version of kube-ai
-	Version = "0.1.8"
+	Version = "0.2.0"
 
 	// GitCommit is the git commit that was compiled
-	GitCommit = "01f2a34"
+	GitCommit = "33cf138"
 
 	// BuildDate is the date when the binary was built
-	BuildDate = "2025-04-10T14:56:58Z"
+	BuildDate = "2025-04-11T00:24:29Z"
 )
